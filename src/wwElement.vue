6b74892667a4995a9009cf787530b98403710c5d
Michel <michel.wu@pwc.com>
2024-11-01 11:17:29 +0100

    first commit

---
+++ src/wwElement.vue
@@ -0,0 +1,40 @@
+<template>
+  <wwLayout
+    class="ww-flexbox"
+    path="children"
+    :direction="content.direction"
+    :disable-edit="isFixed"
+    ww-responsive="wwLayout"
+    :tag="tag"
+    v-bind="properties"
+    :class="{ '-link': hasLink && !isEditing }"
+  >
+    <template #header>
+      <wwBackgroundVideo
+        v-if="backgroundVideo"
+        :video="backgroundVideo"
+      ></wwBackgroundVideo>
+    </template>
+    <template #default="{ item, index, itemStyle }">
+      <wwElement
+        :key="content.children[index][content.propKey] || index"
+        v-bind="item"
+        :extra-style="itemStyle"
+        class="ww-flexbox__object"
+        :ww-responsive="`wwobject-${index}`"
+        :data-ww-flexbox-index="index"
+        @click="onElementClick"
+      ></wwElement>
+    </template>
+  </wwLayout>
+</template>
+<script>
+export default {
+  props: {
+    content: { type: Object, required: true },
+  },
+  computed: {},
+  mounted() {},
+  methods: {},
+};
+</script>
---
+++ ww-config.js
@@ -0,0 +1,61 @@
+export default {
+  options: {
+    lazyHydrate: true,
+    displayAllowedValues: [
+      "flex",
+      "block",
+      "grid",
+      "inline-flex",
+      "inline-block",
+      "inline-grid",
+    ],
+    linkable: true,
+  },
+  inherit: [{ type: "ww-layout" }, { type: "ww-background-video" }],
+  editor: {
+    label: {
+      en: "Flexbox",
+    },
+    icon: "border",
+    bubble: {
+      icon: "border",
+    },
+    customStylePropertiesOrder: ["children"],
+  },
+  properties: {
+    uniqueKey: {
+      label: { en: "keyProp" },
+      type: "Text",
+      defaultValue: "id",
+      bindable: true,
+    },
+    children: {
+      label: {
+        en: "Items",
+        fr: "Items",
+      },
+      type: "Info",
+      options: {
+        text: { en: "Elements to repeat" },
+      },
+      hidden: (content, sidePanelContent, boundProps, wwProps) =>
+        !!(wwProps && wwProps.isFixed),
+      bindable: "repeatable",
+      defaultValue: [],
+      /* wwEditor:start */
+      bindingValidation: {
+        validations: [
+          {
+            type: "array",
+          },
+          {
+            type: "object",
+          },
+        ],
+        tooltip:
+          'A collection or an array of data: \n\n`myCollection` or `[{}, {}, ...] || ["string1", "string2", ...] || [1, 2, ...]`',
+      },
+      /* wwEditor:end */
+    },
+  },
+};
